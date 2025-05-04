eebf947526a7eb303245704dd6320e27d4bba093
jeanalves15 <jeanstella05@gmail.com>
2025-05-04 17:20:29 -0300

    NovoCommit

--- Services/Autor/IAutorInterface.cs
+++ Services/Autor/IAutorInterface.cs
@@ -6,6 +6,11 @@ namespace Livro_Autores_WebAPI8.Services.Autor
     {
         Task<ResponseModel<List<AutorModel>>> ListarAutores();
         Task<ResponseModel<AutorModel>> BuscarAutorID(int idAutor);
-        Task<ResponseModel<AutorModel>> BuscarAutorIdLivro(int idLivro);
+        Task<ResponseModel<AutorModel>> BuscarAutorIdLivro(int idLivro); 
+        Task<ResponseModel<AutorModel>> ExcluirAutor(int idAutor);
+        Task<ResponseModel<AutorModel>> EditarAutor(int idLivro);  
+
+
     }
+}
 }
